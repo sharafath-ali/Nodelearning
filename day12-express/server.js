cfbc8b322dad379bca5a2b6442df87adc0df2484
sharafath ali <sharafathaliofficial@gmail.com>
2023-09-02 20:52:48 +0530

    express intro completed

--- day12-express/server.js
+++ day12-express/server.js
@@ -17,8 +17,40 @@ app.get("/old-page(.html)?", (req, res) => {
    res.redirect(301,"/new-page.html"); //301 is real status code for permenent redirect
 });
 
+//chaining method 1
+app.get('/hello(.html)?', (req,res,next)=>{
+   res.send('hello')
+    next()
+ },(req,res)=>{
+    
+    console.log('attempted to log hello.html ')
+ })
+ 
+
+ //chaining method 2
+
+ const one=(req,res,next)=>{
+   console.log('one');
+   next()
+ }
+
+ const two=(req,res,next)=>{
+   console.log('two')
+   next()
+ }
+ 
+ const three=(req,res)=>{
+   console.log('three')
+   res.send('finished')
+ }
+
+ app.get('/chain(.html)?',[one,two,three])
+ 
+
 app.get("/*", (req, res) => {
    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
